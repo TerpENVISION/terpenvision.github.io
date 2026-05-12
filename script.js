a35bbf2b90e7751781f25b19744a4409ed394841
JoshNotChicken <joshloo619@gmail.com>
2026-05-12 23:42:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const APIURL = "https://api.github.com/users/TerpENVISION/repos"
 console.log(APIURL)
 
 const menuContent = document.getElementById("menucontent")
-const menuContent1 = document.getElementById("menucontent1")
 
 isShow = 0
+
 
 function showBar() {
     if (isShow == 0) {
